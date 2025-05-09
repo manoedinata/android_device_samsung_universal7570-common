41085286205be82911b162ed8a5d59ec69fbdb57
Hendra Manudinata <hendra@manoedinata.com>
2025-05-09 08:00:05 +0000

    universal7570: init: Import stock changes from J400FXXS9CVE4

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,10 +15,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.samsungexynos7870
+LOCAL_MODULE       := fstab.samsungexynos7570
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.samsungexynos7870
+LOCAL_SRC_FILES    := etc/fstab.samsungexynos7570
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
@@ -47,18 +47,18 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.samsungexynos7870.rc
+LOCAL_MODULE       := init.samsungexynos7570.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.samsungexynos7870.rc
+LOCAL_SRC_FILES    := etc/init.samsungexynos7570.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.samsungexynos7870.usb.rc
+LOCAL_MODULE       := init.samsungexynos7570.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.samsungexynos7870.usb.rc
+LOCAL_SRC_FILES    := etc/init.samsungexynos7570.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
